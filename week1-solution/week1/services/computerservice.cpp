f985d466c2f09fdd970a22437d7c263ad616e5a4
Tinna <tinnath@Tinnas-MacBook-Pro.local>
2015-12-07 13:27:26 +0000

    computerservice klasi

--- week1-solution/week1/services/computerservice.cpp
+++ week1-solution/week1/services/computerservice.cpp
@@ -1,7 +1,31 @@
-#include "computerservice.h"
+#include "services/computerservice.h"
+#include "utilities/computercomparator.h"
+
+#include <algorithm>
+
+using namespace std;
+
 
 Computerservice::Computerservice()
 {
 
 }
 
+std::vector<Computer> ComputerService::getAllComputers(std::string orderBy, bool orderAscending)
+{
+    vector<Computer> computers = comuterRepo.getAllComputers();
+
+    std::sort(computers.begin(), computers.end(), ComputerComparator(orderBy, orderAscending));
+
+    return computers;
+}
+
+std::vector<Computer> ComputerService::searchForComputers(std::string searchTerm)
+{
+    return computerRepo.searchForComputers(searchTerm);
+}
+
+bool ComputerService::addComputer(Computer computer)
+{
+    return computerRepo.addComputer(computer);
+}
--- week1-solution/week1/services/computerservice.h
+++ week1-solution/week1/services/computerservice.h
@@ -1,11 +1,19 @@
 #ifndef COMPUTERSERVICE_H
 #define COMPUTERSERVICE_H
-
+#include "repositories/computerrepository.h"
 
 class Computerservice
 {
 public:
     Computerservice();
+
+    std::vector<Computer> getAllComputers(std::string orderBy, bool orderAscending);
+    std::vector<Compter> searchForComputers(std::string searchTerm);
+
+    bool addComputer(Computer computer);
+
+private:
+    ComputerRepository computerRepo;
 };
 
 #endif // COMPUTERSERVICE_H
