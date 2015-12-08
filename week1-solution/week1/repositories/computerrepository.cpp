c594de308b3a48f3779a19454fbaaa5332c1a933
Tinna <tinnath@Tinnas-MacBook-Pro.local>
2015-12-08 14:27:34 +0000

    skrá fyrir computerrepository

---
+++ week1-solution/week1/repositories/computerrepository.cpp
@@ -0,0 +1,7 @@
+#include "computerrepository.h"
+
+Computerrepository::Computerrepository()
+{
+
+}
+
---
+++ week1-solution/week1/repositories/computerrepository.h
@@ -0,0 +1,11 @@
+#ifndef COMPUTERREPOSITORY_H
+#define COMPUTERREPOSITORY_H
+
+
+class Computerrepository
+{
+public:
+    Computerrepository();
+};
+
+#endif // COMPUTERREPOSITORY_H
