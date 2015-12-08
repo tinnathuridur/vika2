434c2e43702b485cfbfab8bd56d951ed530feaec
tryggvi4 <tryggvi4@gmail.com>
2015-12-08 19:34:14 +0000

    Lagfæringar á create table föllunu,

--- week1-solution/week1/Database/database.cpp
+++ week1-solution/week1/Database/database.cpp
@@ -41,21 +41,23 @@ void Database::createTables()
 {
     QSqlQuery query;
     query.exec("CREATE TABLE IF NOT EXISTS Computers"
-               "(`ID_computer` int(11) NOT NULL UNIQUE PRIMARY KEY,"
+               "(`ID_computer` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"
                "`Name` VarChar(100) NOT NULL ,"
                "`BuildYear` int(11),"
                " `ComputerType` VARCHAR(100),"
                "`BuiltOrNot` BOOLEAN)");
 
     query.exec("CREATE TABLE IF NOT EXISTS Scientists"
-               "(`ID_scientist` int(11) NOT NULL UNIQUE PRIMARY KEY,"
+               "(`ID_scientist`  INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"
                "`Name` VarChar(100) NOT NULL ,"
                "`Sex` VarChar(100),"
                "`YearBirth` int(5),"
                "`YearDeath` int(5))");
 
-    query.exec("CREATE TABLE IF NOT EXISTS ConntectionTable"
-               "(`ID` int NOT NULL,"
+
+
+    query.exec("CREATE TABLE IF NOT EXISTS ConnectionTable"
+               "(`ID` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"
                "`ID_scientist` int(11) NOT NULL,"
                "`ID_computer` int(11),"
                "PRIMARY KEY (ID),"
