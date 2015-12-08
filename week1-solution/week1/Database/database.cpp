db2720bab3bfe278d73a50339e322e51063718e6
Tinna <tinnath@Tinnas-MacBook-Pro.local>
2015-12-08 13:22:58 +0000

    database klasinn endurhugsaður

--- week1-solution/week1/Database/database.cpp
+++ week1-solution/week1/Database/database.cpp
@@ -1,85 +1,23 @@
-#include <QCoreApplication>
-using namespace std;
-
+#include <iostream>
+#include <QtSql>
 #include "database.h"
 
-#include <iostream>
+using namespace std;
 
-Database::Database(char* filename) {
+Database::Database()
+{
+    QSqlDatabase db;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    QString dbName = "ComputerHistory_db.sqlite";
+    db.setDatabaseName(dbName);
 
-    database = NULL;
-    open(filename);
-}
-
-//Database::~Database(char*filename) {
-
-//}
-
-bool Database::open(char *filename) {
-
-    if(sqlite3_open(filename, &database) == SQLITE_OK){
-        return true;
+    if (!db.open())
+    {
+        qDebug() << "Error: connection with database fail";
     }
-    else{
-        return false;
+    else
+    {
+        qDebug() << "Database: connection ok";
     }
 }
 
-vector<vector<string> > Database::query(char *query){
-
-    sqlite3_stmt *statement;
-    vector<vector<string> > results;
-
-    if  (sqlite3_prepare_v2(database, query, -1, &statement, 0) == SQLITE_OK){
-
-        int cols = sqlite3_column_count(statement);
-        int result = 0;
-
-        while(true){
-
-            result = sqlite3_step(statement);
-
-            if(result == SQLITE_ROW) {
-
-                vector<string> values;
-                for (int col = 0; col < cols; col++){
-                    string val;
-                    char* ptr = (char*)sqlite3_column_text(statement, col);
-
-                    if (ptr) {
-
-                        val = ptr;
-                    }
-                    else {
-
-                        val = "";
-                    }
-
-                    values.push_back(val);
-                }
-
-                results.push_back(values);
-            }
-
-            else {
-
-                break;
-            }
-        }
-
-        sqlite3_finalize(statement);
-    }
-
-    string error = sqlite3_errmsg(database);
-    if (error != "not an error"){
-
-        cout << query << " " << error << endl;
-    }
-
-    return results;
-}
-
-bool Database::close(char *filename) {
-
-    sqlite3_close(database);
-}
