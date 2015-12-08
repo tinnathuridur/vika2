8734de3963f8b400478d1ed21d7fa1ad25c41cfa
kolbrungun15 <kolbrungun15@ru.is>
2015-12-08 14:12:42 +0000

    Komið inn fall fyrir create table PP Kolbrún og Tinna

--- week1-solution/week1/Database/database.cpp
+++ week1-solution/week1/Database/database.cpp
@@ -35,23 +35,21 @@ void Database::createTables()
                " `ComputerType` VARCHAR(100),"
                "`BuiltOrNot` BOOLEAN)");
 
-    CREATE TABLE IF NOT EXISTS scientists(
-        `ID_scientist` int(11) NOT NULL UNIQUE PRIMARY KEY,
-        `Name` int(11) NOT NULL ,
-        `Sex` VarChar(100),
-        `YearBirth` int(5),
-        `YearDeath` int(5),
-    );
+    query.exec("CREATE TABLE IF NOT EXISTS scientists("
+               "`ID_scientist` int(11) NOT NULL UNIQUE PRIMARY KEY,"
+               "`Name` int(11) NOT NULL ,"
+               "`Sex` VarChar(100),"
+               "`YearBirth` int(5),"
+               "`YearDeath` int(5),)");
 
-    CREATE TABLE IF NOT EXISTS tengiTafla(
-        `ID` int NOT NULL,
-        `ID_scientist` int(11) NOT NULL,
-        `ID_computer` int(11),
-        PRIMARY KEY (ID),
-        UNIQUE (ID_scientist, ID_computer),
-        FOREIGN KEY (ID_computer) REFERENCES computer(ID_computer),
-        FOREIGN KEY (ID_scientist) REFERENCES scientist(ID_scientist)
-    );
+    query.exec("CREATE TABLE IF NOT EXISTS tengiTafla("
+               "`ID` int NOT NULL,"
+               "`ID_scientist` int(11) NOT NULL,"
+               "`ID_computer` int(11),"
+               "PRIMARY KEY (ID),"
+               "UNIQUE (ID_scientist, ID_computer),"
+               "FOREIGN KEY (ID_computer) REFERENCES computer(ID_computer),"
+               "FOREIGN KEY (ID_scientist) REFERENCES scientist(ID_scientist))");
 }
 
 QSqlDatabase Database::getDb()
